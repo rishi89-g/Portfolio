e46d2ad7656563cf37c27e0b0ae6e19cccbcf04c
rishi89-g <rishirajgiri890@gmail.com>
2023-03-13 17:25:55 +0545

    screenshot added

--- lib/sections/topSection/components/person_pic.dart
+++ lib/sections/topSection/components/person_pic.dart
@@ -8,7 +8,7 @@ class PersonPic extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      constraints: BoxConstraints(maxWidth: 639, maxHeight: 1500),
+      constraints: BoxConstraints(maxWidth: 1000, maxHeight: 1500),
       child: Image.asset("assets/images/person.png"),
     );
   }
